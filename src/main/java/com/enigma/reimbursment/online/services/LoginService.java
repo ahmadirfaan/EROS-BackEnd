b72b97a9e9b33e7cd9c5f3e7550b90460c027620
wisa.arsytama <arsytamawisa@gmail.com>
2021-03-12 01:36:22 +0700

    test query

--- src/main/java/com/enigma/reimbursment/online/services/LoginService.java
+++ src/main/java/com/enigma/reimbursment/online/services/LoginService.java
@@ -20,17 +20,7 @@ public class LoginService extends AbstractService<Login, String> {
     }
 
     public Login findByUsernameAndPassword(String username, String password){
-        List<Login> users = loginRepository.findAll();
-        Login data = null;
-
-            for(Login user : users){
-                if(user.getUsername().equals(username) && user.getPassword().equals(password)){
-                    data = user;
-                }
-            }
-
-
-        return data;
+        return loginRepository.findByUsernameAndPassword(username,password);
     }
 
 
